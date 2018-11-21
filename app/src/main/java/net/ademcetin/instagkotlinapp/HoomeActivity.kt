91317f653a032346266b931c33235b223b775c9e
Windows10 <2012029824>
2018-11-21 21:34:39 +0300

    initial commit

---
+++ app/src/main/java/net/ademcetin/instagkotlinapp/HoomeActivity.kt
@@ -0,0 +1,12 @@
+package net.ademcetin.instagkotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HoomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hoome)
+    }
+}
